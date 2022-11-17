c5d7d0a638254a3e8f460a0293262902930ee0e8
Hamdan <85752866+hamdanzulfarais@users.noreply.github.com>
2022-11-17 22:53:57 +0700

    Add files via upload

---
+++ XXI_movie/script.js
@@ -0,0 +1,17 @@
+var p = document.getElementById("popcorn");
+var q = document.getElementById("bread");
+var r = document.getElementById("softdrink");
+var s = document.getElementById("coldtea");
+
+if(p.checked==true){
+    b=b+10000;
+}
+if(q.checked==true){
+    b=b+10000;
+}
+if(r.checked==true){
+    b=b+5000;
+}
+if(s.checked==true){
+    b=b+5000;
+}
